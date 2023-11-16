124bdc73249178e334f93e888150fb5042a91ac4
FiaLDI <leon.youtube2017@yandex.ru>
2023-11-16 16:20:13 +0300

    исправил код

--- prog/ind.py
+++ prog/ind.py
@@ -5,12 +5,23 @@ import sys
 
 
 if __name__ == "__main__":
+    # Колличество винчестеров.
+    N = 22
+    
+    Arr = []
+    while N > 0:
+        # Ввод стоимости
+        x = int(input("Введите стоимость: "))
+        # Ввод веса
+        y = int(input("Введите вес: "))
+        Arr.append([x, y])
+
+    a = tuple(Arr)
+
+    # Искомая соимость
     s = int(input())
 
-    # Винчестеры.
-    a = [(v, p) for v in range(40, 90, 10) for p in range(100, 500, 100)]
-
     # Поиск винчестеров с стоимостью больше s
-    for por, item in enumerate(a):
+    for item in a:
         if item[0] > s:
             print(f"Винчестер: ({item[0]},{item[1]}) - {item[1]}")
