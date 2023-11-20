3f4b38df68a033896fa6ab0341cfa5c82f3fad2c
FiaLDI <leon.youtube2017@yandex.ru>
2023-11-20 12:12:54 +0300

    remake code

--- prog/ind.py
+++ prog/ind.py
@@ -5,12 +5,15 @@
 if __name__ == "__main__":
     # Колличество винчестеров.
     COUNT_OF_VIN = 22
-    
-    list_of_turple = [tuple(map(int, input("Введите стоимость и вес через пробел: ").split())) for i in range(COUNT_OF_VIN)]
 
     # Искомая соимость
     value = int(input("Введите стоимость "))
 
+    list_of_turple = [tuple(map(int, input("Введите стоимость и вес через пробел: ").split())) \
+        for _ in range(COUNT_OF_VIN)]
+
+
     # Поиск винчестеров с стоимостью больше s
-    list_of_rezult = [i for i in list_of_turple if i[0] > value]
+    list_of_rezult = [weight_of_tuple for (value_of_tuple, weight_of_tuple)\
+         in list_of_turple if value_of_tuple > value]
     print(list_of_rezult)
