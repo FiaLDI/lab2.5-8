941797476c3dfb25033943225cd43ea95799adc3
FiaLDI <leon.youtube2017@yandex.ru>
2023-11-20 11:29:26 +0300

    code

--- prog/ind.py
+++ prog/ind.py
@@ -14,14 +14,13 @@ if __name__ == "__main__":
         x = int(input("Введите стоимость: "))
         # Ввод веса
         y = int(input("Введите вес: "))
-        Arr.append([x, y])
-
-    a = tuple(Arr)
+        Arr.append((x,y))
+        N -= 1
 
     # Искомая соимость
     s = int(input())
 
     # Поиск винчестеров с стоимостью больше s
-    for item in a:
+    for item in Arr:
         if item[0] > s:
             print(f"Винчестер: ({item[0]},{item[1]}) - {item[1]}")
